449e9918ac15928e909ac869338e70e5a304c8fe
Bilawal Hussain <bilawal.hussain350@gmail.com>
2026-01-12 12:08:45 +0400

    first commit

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   Github,
   Linkedin,
@@ -9,7 +9,6 @@ import {
   Briefcase,
   GraduationCap,
   Award,
-  ExternalLink,
   Home,
   User,
   Wrench,
